77fa70834e8cea15b8a30a17d4504fff0d7563d8
ayush-ojha-11 <ayushojha1512@gmail.com>
2025-04-09 12:25:56 +0530

    some more changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://chat-app-backend-five-pi.vercel.app/",
+      : "https://chat-app-backend-five-pi.vercel.app/api",
   withCredentials: true,
 });
