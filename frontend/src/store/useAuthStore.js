6f7a3e1b22bde551cc087181ce80040af0f2325a
ayush-ojha-11 <ayushojha1512@gmail.com>
2025-04-09 13:43:51 +0530

    changes

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001"
-    : "https://chat-app-mern-and-socket-io.onrender.com";
+    : "https://chat-app-mern-and-socket-io.onrender.com/api";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
