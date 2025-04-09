28f9f05f2b26947d7826a55d090118d023430460
ayush-ojha-11 <ayushojha1512@gmail.com>
2025-04-09 13:10:15 +0530

    new

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,9 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001"
+    : "https://chat-app-backend-five-pi.vercel.app";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
